1a623bf5e18ee3bf15a3aad8175276254ad4c2d9
Svetlana Shestakova <svetlana.magnifico@gmail.com>
2020-06-12 17:24:34 +0200

    Add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import * as data from './components/product-overview/products';
+import ProductPage from './components/product-page/ProductPage';
 
 interface AppProps {}
 
 function App({}: AppProps) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Link to="/products">Products</Link>
+      <Route path="/products" component={ProductPage}/>
+    </Router>
   );
 }
 
