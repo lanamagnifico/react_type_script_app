a353a0015637c56886a02f779ff6f27f41575c71
Svetlana Shestakova <svetlana.magnifico@gmail.com>
2020-06-12 18:30:44 +0200

    Add material UI. Change product card to row

---
+++ src/components/product/ProductRow.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Product from './Product';
+import Grid from '@material-ui/core/Grid';
+
+interface Props {
+  product: Product;
+};
+
+const ProductRow: React.FunctionComponent<Props> = (props) => {
+  return (
+    <Grid container justify="flex-start" spacing={1} >
+      <Grid item spacing={5}>{props.product.id}</Grid>
+      <Grid item spacing={5}>{props.product.name}</Grid>
+    </Grid>
+  )
+};
+  
+export default ProductRow;
